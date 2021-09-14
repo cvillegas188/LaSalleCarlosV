e9d9c39a16d7daafa8e0bb770bb08a1c488255de
cvillegas188 <78743435+cvillegas188@users.noreply.github.com>
2021-09-13 21:40:56 -0500

    Rename segs.asm to Rutina_Tiempo.asm

--- Rutina_Tiempo.asm
+++ Rutina_Tiempo.asm
@@ -1,6 +1,6 @@
 #include "p16F628a.inc" ;incluir librerias relacionadas con el dispositivo
 __CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _CP_OFF
-;configuración del dispositivotodo en OFF y la frecuencia de oscilador
+;configuraciÃ³n del dispositivotodo en OFF y la frecuencia de oscilador
 ;es la del "reloj del oscilador interno" (INTOSCCLK)
 RES_VECT CODE 0x0000 ; processor reset vector
 GOTO START ; go to beginning of program
